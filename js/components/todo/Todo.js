149d0032919f88cbad0d6e09309de4586c54729b
Sam DOMINGER <sdominger@gmail.com>
2023-09-21 12:58:30 +0200

    Récupération du template de l'app

---
+++ js/components/todo/Todo.js
@@ -0,0 +1,8 @@
+export default class {
+    constructor(data){
+        this.id = data.id;
+        this.content = data.content;
+        this.completed = data.completed;
+        this.createdAt = data.createdAt;
+    }
+}
